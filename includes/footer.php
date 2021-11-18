02ee4598dc1497f35515b6980fb75a36162cb139
Lor3nzOo <87445229+Lor3nzOo@users.noreply.github.com>
2021-11-18 09:31:16 +0100

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,14 @@
+<footer>
+    <div class="container-fluid m-0 p-0 text-center">
+        <div class="row m-0 p-0">
+            <p class="lead copy">
+                Copyright Lor3nzo 2021&copy;
+            </p>
+        </div>
+    </div>
+</footer>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
